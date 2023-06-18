f1606b109c7f20723ed8dee490d3e0552c9791d9
Semtomer <eyisa_51@hotmail.com>
2023-06-18 18:08:49 +0300

    Resources Manager
    Added resources manager script. This script checks at the cost of the buildings according to the resources the player has and makes them enabled or disabled. Also gold image asset revised and building names added.

---
+++ 2D_Building_Idle_Game/Assets/Scripts/ResourcesManager.cs
@@ -0,0 +1,122 @@
+
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ResourcesManager : MonoBehaviour
+{
+    [Header("Owned Resources")]
+    [SerializeField] int ownedGold;
+    [SerializeField] int ownedGem;
+
+    [Header("Owned Resources Text")]
+    [SerializeField] TMP_Text ownedGold_text;
+    [SerializeField] TMP_Text ownedGem_text;
+
+    //Building Costs Variables
+    #region
+    [Header("Building Costs")]
+    [SerializeField] private int pawnGoldCost;
+    [SerializeField] private int pawnGemCost;
+    [SerializeField] private int houseGoldCost;
+    [SerializeField] private int houseGemCost;
+    [SerializeField] private int castleGoldCost;
+    [SerializeField] private int castleGemCost;
+    [SerializeField] private int carGoldCost;
+    [SerializeField] private int carGemCost;
+    [SerializeField] private int sailboatGoldCost;
+    [SerializeField] private int sailboatGemCost;
+    [SerializeField] private int trainGoldCost;
+    [SerializeField] private int trainGemCost;
+    #endregion
+
+    //Building Gold Cost Texts Variables
+    #region
+    [Header("Building Gold Costs Text")]
+    [SerializeField] private TMP_Text pawnGoldCost_text;
+    [SerializeField] private TMP_Text houseGoldCost_text;
+    [SerializeField] private TMP_Text castleGoldCost_text;
+    [SerializeField] private TMP_Text carGoldCost_text;
+    [SerializeField] private TMP_Text sailboatGoldCost_text;
+    [SerializeField] private TMP_Text trainGoldCost_text;
+
+    #endregion
+
+    //Building Gem Cost Texts Variables
+    #region
+    [Header("Building Gem Costs Text")]
+    [SerializeField] private TMP_Text pawnGemCost_text;
+    [SerializeField] private TMP_Text houseGemCost_text;
+    [SerializeField] private TMP_Text castleGemCost_text;
+    [SerializeField] private TMP_Text carGemCost_text;
+    [SerializeField] private TMP_Text sailboatGemCost_text;
+    [SerializeField] private TMP_Text trainGemCost_text;
+    #endregion
+
+    //Transparency Variables for Cards
+    #region
+    [Header("Cards")]
+    [SerializeField] private GameObject pawnCard;
+    [SerializeField] private GameObject houseCard;
+    [SerializeField] private GameObject castleCard;
+    [SerializeField] private GameObject carCard;
+    [SerializeField] private GameObject sailboatCard;
+    [SerializeField] private GameObject trainCard;
+    #endregion
+
+    int[] costGoldList;
+    TMP_Text[] costGoldTextsList;
+
+    int[] costGemList;
+    TMP_Text[] costGemTextsList;
+
+    GameObject[] cardList;
+
+    void Awake()
+    {
+        costGoldList = new int[] { pawnGoldCost, houseGoldCost, castleGoldCost, carGoldCost, sailboatGoldCost, trainGoldCost };
+        costGoldTextsList = new TMP_Text[] { pawnGoldCost_text, houseGoldCost_text, castleGoldCost_text, carGoldCost_text, sailboatGoldCost_text, trainGoldCost_text };
+
+        costGemList = new int[] { pawnGemCost, houseGemCost, castleGemCost, carGemCost, sailboatGemCost, trainGemCost };
+        costGemTextsList = new TMP_Text[] { pawnGemCost_text, houseGemCost_text, castleGemCost_text, carGemCost_text, sailboatGemCost_text, trainGemCost_text };
+
+        cardList = new GameObject[] { pawnCard, houseCard, castleCard, carCard, sailboatCard, trainCard };
+    }
+
+    void Start()
+    {       
+        for (int i = 0; i < costGoldTextsList.Length; i++) 
+        {
+            costGoldTextsList[i].SetText(costGoldList[i].ToString());
+        }
+
+        for (int i = 0; i < costGemTextsList.Length; i++)
+        {
+            costGemTextsList[i].SetText(costGemList[i].ToString());
+        }
+
+        StartCoroutine(RunRepeatedly(1f));
+    }
+
+    void CheckAffordBuilding() 
+    {
+        ownedGold_text.SetText(ownedGold.ToString());
+        ownedGem_text.SetText(ownedGem.ToString());
+
+        for (int i = 0; i < cardList.Length; i++)
+        {
+            if ( ownedGold >= costGoldList[i] && ownedGem >= costGemList[i] )
+               cardList[i].SetActive(false);
+            else 
+               cardList[i].SetActive(true);
+        }
+    }
+
+    IEnumerator RunRepeatedly(float waitTime)
+    {
+        Debug.Log("çalýþtý");
+        CheckAffordBuilding();
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(RunRepeatedly(1f));
+    }
+}
